c5ccffcac3722a9f6aed4c2e9068f2784efe2470
Trashboxez <solovey1000@gmail.com>
2024-05-20 15:08:13 +0300

    fixed comments in hw-7

--- terraform/modules/vpc/output.tf
+++ terraform/modules/vpc/output.tf
@@ -12,3 +12,11 @@ output "public_subnets_id" {
 output "private_subnets_id" {
   value = [for sub in aws_subnet.private_subnets[*] : sub.id]
 }
+
+output "public_subnets_cidrs" {
+  value = aws_subnet.public_subnets[*].cidr_block
+}
+
+output "private_subnets_cidrs" {
+  value = aws_subnet.private_subnets[*].cidr_block
+}
